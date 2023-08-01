89cbef2a86820c91edab7aa58371692c4f157914
Pablo Smolak <pablosmolak2004@hotmail.com>
2023-07-31 22:23:56 -0400

    Aula dia 31-07-2023

---
+++ Frameworkks-para-frontend/app-transportes/src/components/Cabecalho/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css"
+export default function Cabecalho(){
+    
+    return(
+        <header>
+            <h1 className={styles.titulo}>
+                App de transporte
+            </h1>
+        </header>
+    )
+}
---
+++ Frameworkks-para-frontend/app-transportes/src/components/Rodape/index.jsx
@@ -0,0 +1,14 @@
+import styles from "./styles.module.css"
+
+export default function Rodape(){
+
+    return(
+        <>
+            <footer>
+                <h1 className={styles.rodape}>
+                    Aqui é o rodapé do transporte
+                </h1>
+            </footer>
+        </>
+    )
+}
