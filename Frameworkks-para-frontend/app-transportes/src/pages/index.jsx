76ce9b6ca8b81448084e4f28eaa894a7b9f1518e
Pablo Smolak <pablosmolak2004@hotmail.com>
2023-08-03 21:17:02 -0400

    Aula dia 03-08-2023

---
+++ Frameworkks-para-frontend/app-transportes/src/pages/index.jsx
@@ -0,0 +1,19 @@
+import Cabecalho from "@/components/Cabecalho";
+import Card from "@/components/Card";
+import Menu from "@/components/Menu";
+import Rodape from "@/components/Rodape";
+
+export default function Home() {
+  return (
+    <>
+     <Cabecalho />
+     <Menu/>
+     <h1>Página Inicial</h1>
+     <Card titulo="Título 1" texto="Texto do Título 1"/>
+     <Card titulo="Título 2" texto="Texto do Título 2"/>
+     <Card titulo="Título 3" texto="Texto do Título 3"/>
+     <Card titulo="Título 4" texto="Texto do Título 4"/>
+     <Rodape />
+    </>
+  )
+}
