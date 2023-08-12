78cd0f4357b7a14ab336142f1a39090a9c360a98
Pablo Smolak <pablosmolak2004@hotmail.com>
2023-08-11 21:07:31 -0400

    aula 11-08-2023

---
+++ Estrutura-de-dados-II/Bubble Sort/BubbleSort.java
@@ -0,0 +1,37 @@
+public class BubbleSort {
+
+    public static void bubbleSort(int[] num){   
+        Boolean trocou = false;
+        int iteracao = 0;
+        for (int i = 0; i < num.length; i++) {
+            iteracao += 1;
+            trocou = false;
+            for (int j = 0; j < num.length -i-1; j++) {
+                if(num[j]>num[j+1]){
+                    int temp = num[j];
+                    num[j] = num[j+1];
+                    num[j+1] = temp;
+                    trocou = true;
+
+                }
+            }
+
+            if(!trocou){
+                break;
+            }
+        }
+        System.out.println("Iterações: " + iteracao);
+    }
+    public static void main(String[] args) {
+        
+        int[]num = {10,2,3,4,5};
+
+        bubbleSort(num);
+
+
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(num[i]);   
+        }
+    }
+    
+}
