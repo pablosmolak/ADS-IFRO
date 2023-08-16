037eb2d280b04129d37b3c88eb4a14d228e1cf12
Pablo Smolak <pablosmolak2004@hotmail.com>
2023-08-16 15:37:04 -0400

    Atividade de teste de software II

--- Teste-de-software-II/Projeto-Cypress/testes-e2e-cypress-main/cypress/e2e/formularioCadastro.cy.js
+++ Teste-de-software-II/Projeto-Cypress/testes-e2e-cypress-main/cypress/e2e/formularioCadastro.cy.js
@@ -4,14 +4,45 @@ describe('Formulário de Cadastro', () => {
         cy.visit('http://localhost:3000')
     })
 
-    it('O Usuário deve conseguir se cadastrar com sucesso!', () => {
+    it('Deve apresentar erro de Campo Nome obrigatório', () => {
+        cy.getByData('botao-cadastro').click()
+        cy.getByData('email-input').type('ssmolak@gmail.com')
+        cy.getByData('senha-input').type('12345678')
+        cy.getByData('botao-enviar').click()
+        cy.getByData('mensagem-erro').should('exist').and('have.text','O campo de nome é obrigatório')
+    })
+
+    it('Deve apresentar erro de Campo E-mail obrigatório', () => {
+        cy.getByData('botao-cadastro').click()
+        cy.getByData('senha-input').type('12345678')
+        cy.getByData('botao-enviar').click()
+        cy.getByData('mensagem-erro').should('exist').and('have.text','O campo email é obrigatório')
+    })
+
+    it('Deve apresentar erro de Campo Senha obrigatorio', () => {
+        cy.getByData('botao-cadastro').click()
+        cy.getByData('botao-enviar').click()
+        cy.getByData('mensagem-erro').should('exist').and('have.text','O campo de senha é obrigatório')
+    })
+    
+    it('Deve apresentar erro de E-mail já cadastrado', () => {
         cy.getByData('botao-cadastro').click()
         cy.getByData('nome-input').type('smolak')
-        cy.getByData('email-input').type('smolak1fff14@gmail.com')
+        cy.getByData('email-input').type('sssssssssssss@gmail.com')
+        cy.getByData('senha-input').type('12345678')
+        cy.getByData('checkbox-input').click()
+        cy.getByData('botao-enviar').click()
+        cy.getByData('mensagem-erro').should('exist').and('have.text','E-mail já cadastrado!')
+
+    })
+
+    it('O Usuário deve conseguir se cadastrar com sucesso', () => {
+        cy.getByData('botao-cadastro').click()
+        cy.getByData('nome-input').type('smolak')
+        cy.getByData('email-input').type('sssssssssssss@gmail.com')
         cy.getByData('senha-input').type('12345678')
         cy.getByData('checkbox-input').click()
         cy.getByData('botao-enviar').click()
         cy.getByData('mensagem-sucesso').should('exist').and('have.text','Usuário cadastrado com sucesso!')
-
-  })
+    })
 })
---
+++ Teste-de-software-II/Projeto-Cypress/testes-e2e-cypress-main/cypress/e2e/formularioLogin.cy.js
@@ -0,0 +1,36 @@
+describe(('Formulário de Login'), () => {
+
+    beforeEach(() => {
+        cy.visit('http://localhost:3000')
+    })
+
+    it('Deve apresentar erro de Campo E-mail obrigatório', () => {
+        cy.getByData('botao-login').click()
+        cy.getByData('senha-input').type('12345678')
+        cy.getByData('botao-enviar').click()
+        cy.getByData('mensagem-erro').should('exist').and('have.text','O campo email é obrigatório')
+    })
+
+    it('Deve apresentar erro de Campo Senha obrigatório', () => {
+        cy.getByData('botao-login').click()
+        cy.getByData('email-input').type('ssmolak@gmail.com')
+        cy.getByData('botao-enviar').click()
+        cy.getByData('mensagem-erro').should('exist').and('have.text','O campo de senha é obrigatório')
+    })
+
+    it('Deve apresentar erro de E-mail ou senha incorretos', () => {
+        cy.getByData('botao-login').click()
+        cy.getByData('email-input').type('ssmolak@gmail.com')
+        cy.getByData('senha-input').type('12345678')
+        cy.getByData('botao-enviar').click()
+        cy.get('span').should('exist').and('have.text','E-mail ou senha incorretos')
+    })
+
+    it('O usuário deve Logar com sucesso', () => {
+        cy.getByData('botao-login').click()
+        cy.getByData('email-input').type('smolak@gmail.com')
+        cy.getByData('senha-input').type('12345678')
+        cy.getByData('botao-enviar').click()
+    })
+    
+})
