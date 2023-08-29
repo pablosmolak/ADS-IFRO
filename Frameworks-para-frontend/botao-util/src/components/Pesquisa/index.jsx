f81adb58af94a25ae515377167d07139c635dc24
Pablo Smolak <pablosmolak2004@hotmail.com>
2023-08-28 21:49:10 -0400

    Aula 28-08-2023

--- Frameworks-para-frontend/botao-util/src/components/Pesquisa/index.jsx
+++ Frameworks-para-frontend/botao-util/src/components/Pesquisa/index.jsx
@@ -1,35 +1,39 @@
 import Botao from "../Botao"
 import style from "./style.module.css"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export default function Pesquisa() {
 
-    const [pesquisa, setPesquisa] = useState("")
+    const [pesquisa, setPesquisa] = useState(true)
 
-    function enviar(e){
-        e.preventDefault()
-        console.log(`teste: ${pesquisa}!`)
-    }
+
+    useEffect(() => {
+        if(pesquisa ==  true || pesquisa == false){
+            setTimeout(()=>{
+                setPesquisa()
+            }, 1000)
+        }
+    },[pesquisa])
 
     return(
         <>
             <div className={style.pesquisa}>
-                {pesquisa == "" ? 
+                {pesquisa != true && pesquisa != false? 
                     <div className={style.pesquisa_card}>
 
                         <p className={style.card_titulo}>Esse Conteúdo foi útil?</p>
 
-                        <form onSubmit={e => enviar(e)} className={style.pesquisa_form}>
-                            <Botao style={{color:'#0cf249', 'border-color':'#0cf249', width: "90px"}} onClick={e => setPesquisa("Sim")}>😄Sim!</Botao>         
-                            <Botao style={{color:'#cf0606', 'border-color':'#cf0606', width: "90px"}} onClick={e => setPesquisa("Não")}>🙁Não!</Botao>
-                        </form>
+                        <div className={style.pesquisa_form}>
+                            <Botao style={{color:'#0cf249', 'border-color':'#0cf249', width: "90px"}} onClick={e => setPesquisa(true)}>😄Sim!</Botao>         
+                            <Botao style={{color:'#cf0606', 'border-color':'#cf0606', width: "90px"}} onClick={e => setPesquisa(false)}>🙁Não!</Botao>
+                        </div>
 
                     </div>
 
                     :
                 
                     <div className={style.pesquisa_card}>
-                        <div className={style.card_texto}>
+                        <div className={style.card_agradecimento}>
                             <p className={style.card_titulo}>Obrigado!</p>
                             <p className={style.card_texto}>Sua opinião é muito importante para melhorarmos a qualidade de nossos conteúdos!</p>
                         </div>
